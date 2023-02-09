8ba75bb9877049df02bb9da379c984997537f61d
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-02-09 00:13:02 -0300

    publish

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  styledComponents: true,
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
--- src/pages/calculator-challenge-5-bora-codar/index.tsx
+++
@@ -1,5 +0,0 @@
-import Home from '..'
-
-export default function PageHome() {
-  return <Home />
-}
