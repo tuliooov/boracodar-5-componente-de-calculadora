d59c0eed5ce2119cc079ac212ea6be0aef5eb540
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-02-09 00:16:16 -0300

    publish

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   styledComponents: true,
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  basePath: '/one-chat-challenge-4-bora-codar',
+  assetPrefix: '/one-chat-challenge-4-bora-codar',
 }
 
 module.exports = nextConfig
